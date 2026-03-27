de77ea82b1b761cd89babd10e1739bd9cc70c7a8
Carlos <123418797+Cegr7150@users.noreply.github.com>
2026-03-27 14:47:26 -0600

    worked on godot assignment 4

--- Godot/caller.gd
+++ Godot/caller.gd
@@ -6,7 +6,13 @@ func _ready() -> void:
 		print ("hello friend")
 		Receiver.OnCalled()
 
-
+var attack_held := false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	_attack(delta)
 	pass
+
+func _attack(_delta: float) -> void:
+	if Input.is_action_just_pressed("Attack"):
+		attack_held = true
+		print("Attack!!")
